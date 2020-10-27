ded3458c0b8f946c1424bcdecf091b8993b77c82
Diogo Correia <me@diogotc.com>
2020-10-27 14:08:00 +0000

    First commit
    Signed-off-by: Diogo Correia <me@diogotc.com>

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const cron = require('node-cron');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const content = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'content.json'), 'utf-8'));
+
+console.log(content);
+
+const sendEmbed = ({ webhookLink, className, color, id, password, link, comment }) => {
+  const webhookContent = {
+    content: 'Zoom Meeting Information',
+    embeds: [
+      {
+        title: className || 'Unknown class',
+        description: comment || 'Meeting Information',
+        url: link,
+        color: parseInt(color.replace('#', ''), 16),
+        fields: [],
+      },
+    ],
+  };
+  if (id)
+    webhookContent.embeds[0].fields.push({
+      name: 'Meeting ID',
+      value: id,
+      inline: true,
+    });
+  if (password)
+    webhookContent.embeds[0].fields.push({
+      name: 'Meeting Password',
+      value: password,
+      inline: true,
+    });
+
+  axios.post(webhookLink, webhookContent);
+};
+
+Object.entries(content).forEach(([link, courses]) => {
+  courses.forEach((course) => {
+    course.classes.forEach((classObj) => {
+      classObj.time.split(';').forEach((classTime) => {
+        const [weekday, time] = classTime.split(',');
+        const [hour, minute] = time.split(':');
+        cron.schedule(
+          `${minute} ${hour} * * ${weekday}`,
+          () =>
+            sendEmbed({
+              webhookLink: link,
+              className: course.title,
+              color: course.color,
+              ...classObj,
+            }),
+          { timezone: 'Europe/Lisbon' }
+        );
+      });
+    });
+  });
+});
